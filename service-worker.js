bca1baaef4704cc6b7ad2129789075e948b08a2e
Vturnus <Vturnus@users.noreply.github.com>
2021-08-18 11:55:47 +0000

    deploy: 224235ab4eae9f3fb15ce9fee2b298f31af88910

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://Vturnus.github.io/task-management-frontend /precache-manifest.a77503dd82501c3600ca08819b3d9016.js"
+  "https://vturnus.github.io/task-management-frontend/precache-manifest.d88aa161c351490208579001e3696aeb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://Vturnus.github.io/task-management-frontend /index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://vturnus.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
