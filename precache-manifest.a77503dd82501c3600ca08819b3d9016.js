03c918af63c59c9b64dc5a94c47b35cb6a154a37
Vturnus <Vturnus@users.noreply.github.com>
2021-08-18 11:43:27 +0000

    deploy: 224235ab4eae9f3fb15ce9fee2b298f31af88910

---
+++ precache-manifest.a77503dd82501c3600ca08819b3d9016.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8f9f5cac2c1fff185a62be3be8731d",
+    "url": "https://Vturnus.github.io/task-management-frontend /index.html"
+  },
+  {
+    "revision": "30f95f15ea8e474e8842",
+    "url": "https://Vturnus.github.io/task-management-frontend /static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "e6318dfc94f13a927c3b",
+    "url": "https://Vturnus.github.io/task-management-frontend /static/js/2.be4d9c5a.chunk.js"
+  },
+  {
+    "revision": "30f95f15ea8e474e8842",
+    "url": "https://Vturnus.github.io/task-management-frontend /static/js/main.27fb2878.chunk.js"
+  },
+  {
+    "revision": "3ef2133798cd7b018343",
+    "url": "https://Vturnus.github.io/task-management-frontend /static/js/runtime~main.fb098c01.js"
+  }
+]);
